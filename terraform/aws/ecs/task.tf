da3999a02ac78f2f05450b31a41b38f6cdcc246c
Daniel Wu <wuqihua_it@163.com>
2023-02-21 07:21:03 +0000

    commit

---
+++ terraform/aws/ecs/task.tf
@@ -0,0 +1,49 @@
+resource "aws_ecs_task_definition" "test" {
+  family = "service"
+  container_definitions = jsonencode([
+    {
+      name      = "first"
+      image     = "service-first"
+      cpu       = 10
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    },
+    {
+      name      = "second"
+      image     = "service-second"
+      cpu       = 10
+      memory    = 256
+      essential = true
+      portMappings = [
+        {
+          containerPort = 443
+          hostPort      = 443
+        }
+      ]
+    }
+  ])
+
+  volume {
+    name      = "service-storage"
+    host_path = "/ecs/service-storage"
+  }
+}
+resource "aws_ecs_service" "test" {
+  name            = "test"
+  cluster         = aws_ecs_cluster.test.id
+  task_definition = aws_ecs_task_definition.test.arn
+  desired_count   = 3
+#  iam_role        = aws_iam_role.foo.arn
+#  depends_on      = [aws_iam_role_policy.foo]
+
+  ordered_placement_strategy {
+    type  = "binpack"
+    field = "cpu"
+  }
+}
